0fc68365efe437f8b9d6950eb5ec4fe46534f809
Luna <ciubix8514@gmail.com>
2023-03-29 08:30:14 +0300

    Added basic file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #![allow(dead_code)]
+
+use std::path::PathBuf;
+
+use parser::parse_file;
 //Header parser module
 mod parser;
 //module for the iced window
 mod main_window;
 
 fn main() {
-    
+    let data = parse_file(
+        PathBuf::from("/home/luna/Projects/doxygen_gen/testing/mfci_io_70.h"),
+        encoding::all::WINDOWS_1251,
+    )
+    .unwrap();
+    println!("{:?}", data)
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,36 +1,56 @@
-use std::{fs::{ OpenOptions}, path::PathBuf};
-use encoding::{Encoding, DecoderTrap};
+use encoding::{DecoderTrap, Encoding};
+use std::{fs::OpenOptions, io::Read, path::PathBuf};
 
-
-#[derive(Default,Debug,Clone)]
-enum DocumentationType{
+#[derive(Default, Debug, Clone)]
+pub enum DocumentationType {
     #[default]
     Struct,
     Enum,
-    Define
 }
 
-#[derive(Default,Debug,Clone)]
-struct DocumentationItemChild{
-    datatype : String,
-    note : String
+#[derive(Default, Debug, Clone)]
+pub struct DocumentationItemChild {
+    datatype: String,
+    note: String,
 }
 
-#[derive(Default,Debug,Clone)]
-struct DocumentationItem{
-    item_type : DocumentationType,
-    name : String,
-    children : Vec<DocumentationItemChild>
+#[derive(Default, Debug, Clone)]
+pub struct DocumentationItem {
+    item_type: DocumentationType,
+    name: String,
+    children: Vec<DocumentationItemChild>,
 }
 
-
-struct DocumentationData{
-    
+#[derive(Default, Debug, Clone)]
+pub struct DocumentationData {
+    //Potentially add other data here
+    items: Vec<DocumentationItem>,
 }
 
-fn parse_file(path: PathBuf,encoding : Box<dyn Encoding>) -> Result<DocumentationData,std::io::Error>{
-    let file = OpenOptions::new().read(true).open(path)?;
+pub fn parse_file(
+    path: PathBuf,
+    encoding: &dyn Encoding,
+) -> Result<DocumentationData, std::io::Error> {
+    let mut file = OpenOptions::new().read(true).open(path)?;
+    let contents = &mut Vec::<u8>::new();
+    file.read_to_end(contents)?;
 
-    // encoding.decode(DecoderTrap::Ignore).unwrap();
-    todo!();
+    //Decode the file before processing it
+    let contents = encoding.decode(contents, DecoderTrap::Ignore).unwrap();
+
+    let mut data = DocumentationData::default();
+    let mut curret_item: Option<DocumentationItem> = None;
+
+    for line in contents.lines() {
+        //Encountered a struct/enum definition
+        if line.contains("//! ") {
+            let mut i = DocumentationItem::default();
+            i.name = line[4..].to_owned();
+            curret_item = Some(i);
+        }else if let Some(item) = curret_item.as_mut(){
+
+        }
+    }
+
+    Ok(data)
 }
