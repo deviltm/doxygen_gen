073124cc0aee5194fd68c370851595b223a2c00e
Luna <ciubix8514@gmail.com>
2023-03-30 04:33:51 +0300

    fixed regex, fixed tests

--- src/regex.rs
+++ src/regex.rs
@@ -1,22 +1,33 @@
 use regex::Regex;
 pub fn name_regex() -> Regex {
-    Regex::new(r"^typedef\s+(enum|struct)\s+(\w)\s*\{").unwrap()
+    Regex::new(r"typedef\s+(enum|struct)\s+(\w+)\s*\{").unwrap()
 }
 
 #[test]
-fn test_struct_name() {
-    let input = "typedef struct Employee {";
+fn test_struct_name_weird_whitespaces() {
+    let input = "           typedef            struct      employee_struct              {      ";
     let re = name_regex();
+    assert_eq!(re.is_match(input),true);
     let captures = re.captures(input).unwrap();
     assert_eq!(captures.get(1).unwrap().as_str(), "struct");
-    assert_eq!(captures.get(2).unwrap().as_str(), "Employee");
+    assert_eq!(captures.get(2).unwrap().as_str(), "employee_struct");
+}
+#[test]
+fn test_struct_name() {
+    let input = "typedef struct employee_struct {";
+    let re = name_regex();
+    assert_eq!(re.is_match(input),true);
+    let captures = re.captures(input).unwrap();
+    assert_eq!(captures.get(1).unwrap().as_str(), "struct");
+    assert_eq!(captures.get(2).unwrap().as_str(), "employee_struct");
 }
 
 #[test]
 fn test_enum_name() {
-    let input = "typedef enum Employee {";
+    let input = "typedef enum employee_struct {";
     let re = name_regex();
+    assert_eq!(re.is_match(input),true);
     let captures = re.captures(input).unwrap();
     assert_eq!(captures.get(1).unwrap().as_str(), "enum");
-    assert_eq!(captures.get(2).unwrap().as_str(), "Employee");
+    assert_eq!(captures.get(2).unwrap().as_str(), "employee_struct");
 }
